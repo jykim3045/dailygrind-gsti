d9e2a099902e09b7ef77c0ff94f583a5fe6be485
jykim3045 <jykim3045@gmail.com>
2023-06-26 05:10:56 +0900

    hmm

--- js/share.js
+++ js/share.js
@@ -1,6 +1,6 @@
-const url = 'https://dev-dain.github.io/10-things-test';
-const title = '[이미지테스트] 나를 알아보는 10가지 질문 ';
-const hash = '%2310_things_test %233분이미지테스트 %23이미지테스트 ';
+const url = 'https://jykim3045.github.io/dailygrind-gsti/';
+const title = '[GSTI]당신의 최애 2022 국산 스케이트 파트는?';
+const hash = '%GSTI %dailygrind ';
 
 const fb = () => {
   window.open('https://www.facebook.com/sharer/sharer.php?u='
